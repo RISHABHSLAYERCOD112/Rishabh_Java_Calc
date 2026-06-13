4a0e5b47a81d703a34e8907fb496adc1196637fc
Rishabh Kumar Sharma <kpsrishabhsharma@gmail.com>
2026-06-13 14:36:59 +0530

    Update Cal.java

--- calculator/Cal.java
+++ calculator/Cal.java
@@ -1,36 +1,47 @@
 import java.util.*;
-class Calculator{
+
+class Calculator {
     double a;
     double b;
-        Calculator(double a, double b) {
+
+    Calculator(double a, double b) {
         this.a = a;
         this.b = b;
     }
+
     void Add() {
         System.out.println("Addition: " + (a + b));
     } 
+
     void Sub() {
         System.out.println("Subtraction: " + (a - b));
     }
+
     void Mul() {
         System.out.println("Multiplication: " + (a * b));
     }
+
     void Div() {
-        if(b != 0) {
-            System.out.println("Division: " + (a / b));
+        if (b != 0) {
+            System.out.println("Division: " + (a / b)); 
         } else {
             System.out.println("Cannot divide by zero.");
         }
     }
 }
-public class Cal{
-    public static void main(String[]args) {
+
+public class Cal {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
         double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
         double b = sc.nextDouble();
-        Calculator e =new Calculator(a,b);
-        char op='*';
-        switch(op) {
+        
+        Calculator e = new Calculator(a, b);
+        char op = '*'; 
+        switch (op) {
             case '+':
                 e.Add();
                 break;
@@ -41,10 +52,10 @@ public class Cal{
                 e.Mul();
                 break;
             case '/':
-                 e.Div();
+                e.Div();
                 break;
             default:
                 System.out.println("Invalid operator.");
-            }
         }
+    }
 }
